0e5cef1e7713f1a38602f333a85f237192c8e106
mliu1204 <mliu1204@stanford.edu>
2023-11-27 19:34:36 -0800

    Initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,10 @@
+import RootLayout from '../layout';
+
+export default function Page() {
+  return (
+    <RootLayout>
+      {/* Your page content goes here */}
+      <p>Customers Page</p>
+    </RootLayout>
+  );
+}
